6fa75888da4d87926d29fb0bb1c20a2d7e78d27d
Elvin Jin <elvinjin8@gmail.com>
2015-03-29 02:00:41 +0800

    Remote play/pause/stop/previous/next/mute/unmute

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,159 @@
+var tag = document.createElement( 'script' );
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName( 'script' )[ 0 ];
+firstScriptTag.parentNode.insertBefore( tag, firstScriptTag );
 
+var player;
+function onYouTubeIframeAPIReady() {
+
+	player = new YT.Player( 'player', {
+		height : '100%',
+		width : '100%',
+		playerVars: { 'controls' : 0 },
+		videoId : 'M7lc1UVf-VE',
+		events : {
+			// 'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		}
+	} );
+}
+
+window.addEventListener('resize', function() {
+	if (window.innerWidth >= 992) {
+		if (player === null) {
+			player = new YT.Player( 'player', {
+				height : '100%',
+				width : '100%',
+				playerVars: { 'controls' : 0 },
+				videoId : 'M7lc1UVf-VE',
+				events : {
+					// 'onReady': onPlayerReady,
+					'onStateChange': onPlayerStateChange
+				}
+			} );
+		}
+	} else {
+		if (player !== null) {
+			player.destroy();
+		}
+		player = null;
+	}
+});
+
+function onPlayerReady( event ) {
+	// event.target.playVideo();
+}
+
+function onPlayerStateChange ( event ) {
+	if (event.data == YT.PlayerState.ENDED) {
+		player.loadVideoById('xsWN1Wakuec');
+	}
+}
+
+var socket = io();
+////// SEND - START //////
+function broadcastPlay () {
+	socket.emit('play');
+}
+
+function broadcastPause () {
+	socket.emit('pause');
+}
+
+function broadcastStop () {
+	socket.emit('stop');
+}
+
+function broadcastStepBackward () {
+	socket.emit('sb');
+}
+
+function broadcastStepForward () {
+	socket.emit('sf');
+}
+
+function broadcastMute () {
+	socket.emit('mute');
+}
+
+function broadcastUnmute () {
+	socket.emit('unmute');
+}
+////// SEND - END //////
+
+// BACKWARD - START
+function backward () {
+	targetTime = player.getCurrentTime() - 2.0;
+	if (targetTime < 0) {
+		targetTime = 0;
+	}
+	player.seekTo(targetTime);
+}
+
+var backwardTimeOut;
+
+function backwardMouseDown () {
+	backward();
+	backwardTimeOut = setInterval(function(){backward()}, 300);
+}
+
+function backwardMouseUp () {
+	clearInterval(backwardTimeOut);
+}
+// BACKWARD - END
+
+// FORWARD - START
+function forward () {
+	currentTime = player.getCurrentTime();
+	player.seekTo(currentTime+2.0);
+}
+
+var forwardTimeOut;
+
+function forwardMouseDown () {
+	forward();
+	forwardTimeOut = setInterval(function(){forward()}, 300);
+}
+
+function forwardMouseUp () {
+	clearInterval(forwardTimeOut);
+}
+// FORWARD - END
+
+////// RECEIVE - START //////
+socket.on( 'play', function( ) {
+	player.playVideo();
+} );
+
+socket.on( 'pause', function( ) {
+	player.pauseVideo();
+} );
+
+socket.on( 'stop', function( ) {
+	player.stopVideo();
+} );
+
+socket.on( 'sb', function( ) {
+	player.loadVideoById('6Y47qPyoywc');
+} );
+
+socket.on( 'backward', function( ) {
+	player.playVideo();
+} );
+
+socket.on( 'forward', function( ) {
+	player.playVideo();
+} );
+
+socket.on( 'sf', function( ) {
+	player.loadVideoById('RFZrzg62Zj0');
+} );
+
+socket.on( 'mute', function( ) {
+	player.mute();
+} );
+
+socket.on( 'unmute', function( ) {
+	player.unMute();
+} );
+////// RECEIVE - END //////
--- server.js
+++ server.js
@@ -9,9 +9,57 @@ app.get( '/', function ( request, response ) {
 	response.sendFile( __dirname + '/views/index.html');
 } );
 
+app.get( '/test', function ( request, response ) {
+	response.sendFile( __dirname + '/views/indexaa.html');
+} );
+
 var host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 4140;
 
 var server = app.listen( port, host, function () {
 	console.log( 'Listening at http://%s:%s', host, port );
+} );
+
+var io = require( 'socket.io' )( server );
+io.on( 'connection', function( socket ) {
+
+	console.log( 'New user connected' );
+	socket.on( 'disconnect', function() {
+		console.log( 'User disconnected' );
+	} );
+	
+	socket.on( 'play', function( ) {
+		console.log( 'Broadcasting PLAY' );
+		io.emit( 'play' );
+	} );
+	
+	socket.on( 'pause', function( ) {
+		console.log( 'Broadcasting PAUSE' );
+		io.emit( 'pause' );
+	} );
+	
+	socket.on( 'stop', function( ) {
+		console.log( 'Broadcasting STOP' );
+		io.emit( 'stop' );
+	} );
+	
+	socket.on( 'sb', function( ) {
+		console.log( 'Broadcasting STEP-BACKWARD' );
+		io.emit( 'sb' );
+	} );
+	
+	socket.on( 'sf', function( ) {
+		console.log( 'Broadcasting STEP-FORWARD' );
+		io.emit( 'sf' );
+	} );
+	
+	socket.on( 'mute', function( ) {
+		console.log( 'Broadcasting MUTE' );
+		io.emit( 'mute' );
+	} );
+	
+	socket.on( 'unmute', function( ) {
+		console.log( 'Broadcasting UNMUTE' );
+		io.emit( 'unmute' );
+	} );
 } );
